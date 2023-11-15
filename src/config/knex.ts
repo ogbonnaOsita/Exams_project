6f257f14e1e93fdf1bbf5df918309d91f55723e5
ogbonnaOsita21 <smartjosh21@gmail.com>
2023-11-15 13:10:58 +0100

    knex update

--- src/config/knex.ts
+++ src/config/knex.ts
@@ -2,17 +2,23 @@ import Knex from "knex";
 import "dotenv/config";
 
 // const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
-const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+const {
+  DB_HOST_PROD,
+  DB_PORT_PROD,
+  DB_USER_PROD,
+  DB_PASSWORD_PROD,
+  DB_DATABASE_PROD,
+} = process.env;
 
 const knex = Knex({
   client: "postgresql",
   connection: {
-    host: DB_HOST,
-    port: Number(DB_PORT),
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    // ssl: true,
+    host: DB_HOST_PROD,
+    port: Number(DB_PORT_PROD),
+    user: DB_USER_PROD,
+    password: DB_PASSWORD_PROD,
+    database: DB_DATABASE_PROD,
+    ssl: true,
   },
   pool: {
     min: 2,
